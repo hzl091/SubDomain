0c050dc851015bd7b78ed153214869387785581d
hzl091 <hzl091@126.com>
2017-05-25 14:44:28 +0800

    应用程序域操作

---
+++ SubDomain.App/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SubDomain.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(3, 6);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(433, 21);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(442, 5);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "浏览...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(3, 33);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(514, 490);
+            this.treeView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 525);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ SubDomain.App/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubDomain.App
+{
+    public partial class Form1 : Form
+    {
+        //http://www.cnblogs.com/kongyiyun/archive/2011/08/01/2123459.html
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "(*.dll)|*.dll";
+            string p = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "private");
+            foreach (var file in Directory.GetFiles(p, "*.dll", SearchOption.AllDirectories))
+            {
+                File.Delete(file);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult rs = openFileDialog1.ShowDialog();
+            if (rs == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                string dllFileName = Path.GetFileName(path);
+                txtFile.Text = path;
+
+                treeView1.Nodes.Clear();
+                string domainName = Guid.NewGuid().ToString();
+                AppDomain myDomain = this.NewAppDomain(domainName);
+
+                string domainBinPath = Path.Combine(myDomain.RelativeSearchPath, domainName);
+                if (!Directory.Exists(domainBinPath))
+                {
+                    Directory.CreateDirectory(domainBinPath);
+                }
+
+                string currentDllFile = txtFile.Text;
+                string currentDllDir = Path.GetDirectoryName(currentDllFile);
+                string[] allDllFiles = Directory.GetFiles(currentDllDir, "*.dll");
+                foreach (var file in allDllFiles)
+                {
+                    File.Copy(file, Path.Combine(domainBinPath, Path.GetFileName(file)));
+                }
+
+                var files = Directory.GetFiles(Path.Combine(myDomain.RelativeSearchPath, domainName), "*.dll");
+                foreach (var file in files)
+                {
+                    var assembly = Assembly.LoadFrom(file);
+                    assembly.GetReferencedAssemblies();
+
+                    if (file.Contains(dllFileName))
+                    {
+                        Type[] types = assembly.GetTypes();
+                        foreach (var type in types)
+                        {
+                            if (!type.IsClass || type.IsEnum)
+                            {
+                                continue;
+                            }
+                            var q = from p in type.GetMembers()
+                                    where p.MemberType == MemberTypes.Custom || p.MemberType == MemberTypes.Property
+                                    select new TreeNode(p.Name) { Tag = p.GetType().Name };
+                            TreeNode node = new TreeNode(type.Name, q.ToArray());
+                            treeView1.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+        }
+
+        private AppDomain NewAppDomain(string domainName)
+        {
+            AppDomainSetup setup = new AppDomainSetup();
+            setup.ApplicationName = "ApplicationLoader";
+            setup.ApplicationBase = AppDomain.CurrentDomain.BaseDirectory;
+            setup.PrivateBinPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "private");
+            setup.CachePath = setup.ApplicationBase;
+            setup.ShadowCopyFiles = "true";
+            setup.ShadowCopyDirectories = setup.ApplicationBase;
+            AppDomain.CurrentDomain.SetShadowCopyFiles();
+            return AppDomain.CreateDomain(domainName, null, setup);
+        }
+    }
+}
